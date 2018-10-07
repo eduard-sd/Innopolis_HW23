a100ddd86ec70d6c424fb0d0a3c46bdd43f23983
eduard-sd <pr-spektr@yandex.ru>
2018-10-07 20:54:21 +0300

    first commit

---
+++ src/ru/sayakhov/Main.java
@@ -0,0 +1,27 @@
+package ru.sayakhov;
+/**
+ *
+ * Реализовать класс корзины интернет магазина по следующему интерфейсу:
+ *
+ * interface Basket {
+ *
+ *     void addProduct(String product, int quantity);
+ *
+ *     void removeProduct(String product);
+ *
+ *     void updateProductQuantity(String product, int quantity);
+ *
+ *     void clear();
+ *
+ *     List<String> getProducts();
+ *
+ *     int getProductQuantity(String product);
+ *
+ * }
+ * */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
